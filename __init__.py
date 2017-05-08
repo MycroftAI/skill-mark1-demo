d8e35ee3f2033abab22a07dfaa40ca7cf86215fa
Augusto Monteiro 'Sparky <augustomna2010@gmail.com>
2017-05-07 18:26:04 -0700

    Fixing stop, creating a flag to check if should stop and raise a exception

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
-import time
+from threading import Timer
 
+import time
 from mycroft.messagebus.message import Message
 from mycroft.skills.core import MycroftSkill
 
@@ -7,38 +8,50 @@ from mycroft.skills.core import MycroftSkill
 class Mark1DemoSkill(MycroftSkill):
     def __init__(self):
         super(Mark1DemoSkill, self).__init__("Mark1DemoSkill")
-        self.stop = False
+        self.thread = None
+        self.kill = False
 
     def initialize(self):
         self.emitter.on("mycroft.mark1.demo", self.demo)
 
-    def demo(self, message):
+    def sleep(self, t):
+        if self.kill:
+            raise Exception
+        time.sleep(t)
+
+    def mark1(self):
         self.enclosure.mouth_text("Hello, My name is Mark One")
         self.emitter.emit(Message("mycroft.sing"))
         for i in range(0, 10):
-            if self.stop:
-                break
             self.enclosure.eyes_look("l")
-            time.sleep(1)
+            self.sleep(1)
             self.enclosure.eyes_look("u")
-            time.sleep(1)
+            self.sleep(1)
             self.enclosure.eyes_look("d")
-            time.sleep(1)
+            self.sleep(1)
             self.enclosure.eyes_look("r")
-            time.sleep(1)
+            self.sleep(1)
         self.enclosure.eyes_reset()
-        time.sleep(1)
+        self.sleep(1)
         self.speak("It is my first time in the world")
-        time.sleep(3)
+        self.sleep(3)
         self.enclosure.eyes_narrow()
         self.enclosure.mouth_text("Talk with me")
         for i in range(1, 50):
             self.enclosure.eyes_brightness(i)
-            time.sleep(0.1)
+            self.sleep(0.1)
+        self.enclosure.eyes_reset()
+
+    def demo(self, message):
+        try:
+            self.kill = False
+            self.mark1()
+        except:
+            self.enclosure.eyes_reset()
 
     def stop(self):
+        self.kill = True
         self.enclosure.eyes_reset()
-        self.stop = True
 
 
 def create_skill():
